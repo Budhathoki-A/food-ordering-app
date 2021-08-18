b9383e5e869d269388b47540fa73d5016d935ae5
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-08-18 14:06:15 +1000

    modal for cart added with portals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/layout/Header";
 import Meals from "./components/Meals/Meals";
 
 const App = () => {
   return (
     <Fragment>
+      <Cart />
       <Header />
-      <Meals />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
