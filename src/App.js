f7fc0940b9b8f95591a9338e5a14d11e0943b10c
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-08-18 14:42:52 +1000

    moal and cart state is managed upto now using useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Fragment } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/layout/Header";
 import Meals from "./components/Meals/Meals";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
